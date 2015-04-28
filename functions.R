c26cc9d450064fc40ddd689460f4166423f51f35
Hamid R. Darabi <hamid.darabi@groupm.com>
2015-04-28 14:57:15 -0400

    25

--- functions.R
+++ functions.R
@@ -5,3 +5,13 @@ str2   = function(x){ str(x, max.level=2)}
 str3   = function(x){ str(x, max.level=3)}
 
 rmwarn = function(){ assign("last.warning", NULL, envir = baseenv()) }
+
+gline  = function(df){
+        col = rainbow(ncol(df)-1)
+        library(ggplot2)
+        g = ggplot(data=df, aes(x=data[,1],y=data[,2])) + geom_line(color=col[1])
+        for( i in 3:ncol(df)){
+                g = g + geom_line(data=data, aes(y=data[,i]), color=col[i-1])       
+        }
+        g
+}
