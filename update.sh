239bccf2daa8beca19dcb54ff18bdf3e62fb1a44
Hamid R. Darabi, PhD <hamid.darabi@groupm.com>
2016-11-02 17:16:45 -0400

    Snippets are added

--- update.sh
+++ update.sh
@@ -1,7 +1,7 @@
 ##################################################################
 # Name        : update.sh
 # Description : This script automates copying my settings.
-# Version     : 0.0.12
+# Version     : 0.0.13
 # Created On  : 2016-08-30
 # Modified On : 2016-11-02
 # Author      : Hamid R. Darabi, Ph.D.
@@ -12,8 +12,7 @@ git config --global core.autocrlf false
 cp '/c/Users/hamid.darabi/Documents/.R' './dot_r' -r
 cp '/c/Users/hamid.darabi/Documents/.Rprofile' './dot_Rprofile.R'
 cp '/c/Users/hamid.darabi/_vimrc' './dot_vimrc'
-cp '/c/Users/hamid.darabi/.vim/bundle/vim-snippets/snippets'
-'./windows/UltiSnips' -r
+cp '/c/Users/hamid.darabi/.vim/bundle/vim-snippets/snippets' './windows/UltiSnips' -r
 
 git add * --all
 git commit -m "$1"
