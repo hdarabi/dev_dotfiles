fec21ff96cc79c112e29acff63743c7d6e22cedc
Hamid R. Darabi, Ph.D <hdarabi@gmail.com>
2017-04-19 21:26:22 -0400

    python snippet

--- update.sh
+++ update.sh
@@ -1,18 +1,18 @@
 ##################################################################
 # Name        : update.sh
 # Description : This script automates copying my settings.
-# Version     : 0.0.14
+# Version     : 0.0.16
 # Created On  : 2016-08-30
-# Modified On : 2016-11-02
+# Modified On : 2017-04-19
 # Author      : Hamid R. Darabi, Ph.D.
 ##################################################################
 
 git config --global core.autocrlf false
 
-cp '/c/Users/hamid.darabi/Documents/.R' './dot_r' -r
-cp '/c/Users/hamid.darabi/Documents/.Rprofile' './dot_Rprofile.R'
-cp '/c/Users/hamid.darabi/_vimrc' './dot_vimrc'
-cp '/c/Users/hamid.darabi/.vim/bundle/vim-snippets/snippets' './windows/' -r
+cp '/c/Users/R3PUX/Documents/.R' './dot_r' -r
+cp '/c/Users/R3PUX/Documents/.Rprofile' './dot_Rprofile.R'
+cp '/c/Users/R3PUX/_vimrc' './dot_vimrc'
+cp '/c/Users/R3PUX/.vim/bundle/vim-snippets/snippets' './windows/' -r
 
 git add * --all
 git commit -m "$1"
